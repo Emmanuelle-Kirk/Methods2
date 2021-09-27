fed2a52b72c1993af6567c293933702e3e8134be
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-09-27 13:08:51 -0300

    Update Program.cs

--- Methods2/Program.cs
+++ Methods2/Program.cs
@@ -8,9 +8,30 @@ namespace Methods2
 {
     class Program
     {
+        static int health;
+        
+        static void ShowHud()
+        {
+            Console.WriteLine("Health: " + health);
+
+        }
+
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+
+        }
         static void Main(string[] args)
         {
+            health = 100;
+            ShowHud();
+            TakeDamage(25);
+            ShowHud();
+            TakeDamage(15);
+            ShowHud();
+
             //methods 2
+            Console.ReadKey(true);        
         }
     }
 }
